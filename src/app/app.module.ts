d3aac324ba25f14fbf6a1a9be60cc2e6b0df2a03
Patchthesock <stephencallaghan91@gmail.com>
2019-08-01 16:42:05 +0100

    Initial commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { SecureComponent } from './secure/secure.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    SecureComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
