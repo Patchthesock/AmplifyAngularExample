dd45b7d72dfc889521b7cbcc9f843396f73c7140
Patchthesock <stephencallaghan91@gmail.com>
2019-08-01 16:49:06 +0100

    Added App routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component'
+import { AuthComponent } from './auth/auth.component'
+import { SecureComponent } from './secure/secure.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent, pathMatch: 'full' },
+  { path: 'secure', component: SecureComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
